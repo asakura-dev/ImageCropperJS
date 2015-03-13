96990deffe990cde6d4fbfec50a9b3db718660bf
asakura_dev <asakuradev@gmail.com>
2015-03-13 23:53:05 +0900

    要素を追加して操作用のインスタンスを返すぐらいまで書いた

--- src/image-cropper.js
+++ src/image-cropper.js
@@ -1,24 +1,57 @@
-(function($){
+(function ($) {
+  "use strict";
+  var Cropper = function($wrapper){
+    this.$wrapper = $wrapper;
+    this.$canvas = $wrapper.children(".ic_canvas");
+    this.$cover_canvas = $wrapper.children(".ic_cover_canvas");
+    this.$crop = $wrapper.children(".ic_crop");
+  };
+  $.extend(Cropper.prototype,{
+    
+  });
+  
   // constructor
-  var ImageCropper = function(){
+  var ImageCropper = function () {
     //propaties
     // these will be changed by option argumetns of init
-    this.canvas = {
-      width: "500px",
-      height: "300px",
-    };
-    this.crop = {
-      width: "200px",
-      height: "280px",
-    };
-  }
+  };
   // method
   $.extend(ImageCropper.prototype, {
-    init: function(target_element,options){
-      if(options){
-        if(options["canvas"]){
+    init: function (target_element, options) {
+      // default configuration values
+      var config = {
+        canvas: {
+          width: "500px",
+          height: "300px"
+        },
+        crop: {
+          width: "300px",
+          height: "280px"
+        }
+      };
+      // apply option values
+      if (options) {
+        if (options.canvas) {
+          config.canvas = options.canvas;
+        }
+        if (options.crop) {
+          config.crop = options.crop;
         }
       }
+      if (!$(target_element)) {
+        return;
+      } 
+      var $wrapper = $(target_element).eq(0);
+      $wrapper.append('<canvas class="ic_canvas"'+
+                      'width="'+config.canvas.width+'"height="'+config.canvas.height+'">'+
+                      '</canvas>'+
+                      '<canvas class="ic_cover_canvas"'+
+                      'width="'+config.canvas.width+'"height="'+config.canvas.height+'">'+
+                      '</canvas>'+
+                      '<canvas class="ic_crop"'+
+                      'width="'+config.crop.width+'" height="'+config.crop.height+'">'+
+                      '</canvas>');
+      return new Cropper($wrapper);
     }
   });
   $.extend({
